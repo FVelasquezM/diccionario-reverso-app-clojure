fbf8694b3a73fe183a3ae6fb28685807538d445b
Felipe Velasquez <felipe.velasquez@felipe.velasquez>
2023-12-27 12:16:01 -0500

    Added adapter

--- src/diccionario_reverso_app_clojure/adapters/loan.clj
+++ src/diccionario_reverso_app_clojure/adapters/loan.clj
@@ -4,5 +4,4 @@
   "Does a small, document to wire conversion for no practical reason, intended simply as test of the diplomat architecture's adapter."
   [results]
   {:results results
-   :origin "Clojure Server"}
-  results)
+   :origin "Clojure Server"})
